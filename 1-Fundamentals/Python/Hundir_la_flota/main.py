8879d4e04e83a94fb41801a92f7dddc6633387f4
diego-dng <diego.dng.1994@gmail.com>
2025-05-07 15:23:46 +0200

    w3 Hundir la flota v2

--- 1-Fundamentals/Python/Hundir_la_flota/main.py
+++ 1-Fundamentals/Python/Hundir_la_flota/main.py
@@ -1,11 +1,26 @@
 import utils
 import variables as vr
 
+tablero_rival_barcos = utils.crear_tablero()    # MOSTRAR BARCOS DEL RIVAL Y LOS DISPAROS DEL JUGADOR.
+tablero_rival_disparos = utils.crear_tablero()  # MOSTRAR DISPAROS DEL RIVAL.
+
+tablero_jugador_barcos = utils.crear_tablero()  # MOSTRAR BARCOS DEL JUGADOR Y LOS DISPAROS DEL RIVAL.
+tablero_jugador_disparos = utils.crear_tablero()    # MOSTRAR DISPAROS DEL JUGADOR.
 
 
-tablero_main = utils.crear_tablero()
-print(tablero_main)
-print("_________________________")
-tablero_modificado = utils.colocar_barco(tablero_main, vr.barco_1)
-tablero_modificado = utils.colocar_barco(tablero_main, vr.barco_2)
-print(tablero_modificado)
+print("TABLERO DONDE SE MUESTRAN LOS BARCOS DEL RIVAL Y LOS DISPAROS DEL JUGADOR.")
+print(tablero_rival_barcos)
+
+print("TABLERO DONDE SE MUESTRAN LOS DISPAROS DEL RIVAL")
+print(tablero_rival_disparos)
+
+print("___________________________________")
+
+print("TABLERO DONDE SE MUESTRA LOS BARCOS DEL JUGADOR Y LOS DISPAROS DEL RIVAL")
+print(tablero_jugador_barcos)
+
+print("TABLERO DONDE SE MUESTRAN LOS DISPAROS DEL JUGADOR.")
+print(tablero_jugador_disparos)
+
+
+
--- 1-Fundamentals/Python/Hundir_la_flota/utils.py
+++ 1-Fundamentals/Python/Hundir_la_flota/utils.py
@@ -8,8 +8,23 @@ def crear_tablero():
 # barco_1 = [[0,3], [0,4], [0,5], [0,6]]
 # barco_jugador = [[[0,3], [0,4], [0,5], [0,6]], [[4,7], [5,7], [6,7]], [[8,8], [8,9]], [[1,7]]]
 
-def colocar_barco(tablero, barco):
-    for i in barco:
-        tablero[i[0], i[1]] = "O"
+def colocar_barcos(tablero, barcos):
+    for  i in barcos:
+        for j in i:
+            tablero[j[0], j[1]] = "O"
         
+    return tablero
+
+def disparo(tablero):
+    fila = int(input("selecciona la fila"))
+    columna = int(input("selecciona la columna"))
+    if tablero[fila,columna] =="O":
+        tablero[fila,columna] = "X"
+        print("¡¡¡IMPACTOOOOO!!!")
+    elif tablero[fila,columna] == "#" or tablero[fila, columna] == "X":
+        print("Posiccion ya seleccionada")
+        disparo()
+    else:
+        tablero[fila,columna] = "#"
+        print("FALLASTE :)")
     return tablero
